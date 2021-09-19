87863f7f956da900aceb2629e8600bf82f680f17
android156 <n_degtyarev@mail.ru>
2021-09-19 19:58:52 +0300

    initView and setupListeners added

--- app/src/main/java/home/android1/mycalculator/MainActivity.java
+++ app/src/main/java/home/android1/mycalculator/MainActivity.java
@@ -3,12 +3,79 @@ package home.android1.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView resultTv;
+    private Button button_1;
+    private Button button_2;
+    private Button button_3;
+    private Button button_4;
+    private Button button_5;
+    private Button button_6;
+    private Button button_7;
+    private Button button_8;
+    private Button button_9;
+    private Button button_0;
+    private Button button_point;
+    private Button button_add;
+    private Button button_sub;
+    private Button button_mult;
+    private Button button_div;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        setupListeners();
+    }
+    private void initView() {
+// Получить пользовательские элементы по идентификатору
+        resultTv = findViewById(R.id.result_textview);
+        Button button_1 = findViewById(R.id.button_1);
+        Button button_2 = findViewById(R.id.button_2);
+        Button button_3 = findViewById(R.id.button_3);
+        Button button_4 = findViewById(R.id.button_4);
+        Button button_5 = findViewById(R.id.button_5);
+        Button button_6 = findViewById(R.id.button_6);
+        Button button_7 = findViewById(R.id.button_7);
+        Button button_8 = findViewById(R.id.button_8);
+        Button button_9 = findViewById(R.id.button_9);
+        Button button_0 = findViewById(R.id.button_0);
+        Button button_point = findViewById(R.id.button_point);
+        Button button_add = findViewById(R.id.button_add);
+        Button button_mult = findViewById(R.id.button_mult);
+        Button button_div = findViewById(R.id.button_div);
+        Button button_sub = findViewById(R.id.button_sub);
+    }
+
+    private void setupListeners() {
+        button_1.setOnClickListener(this);
+        button_2.setOnClickListener(this);
+        button_3.setOnClickListener(this);
+        button_4.setOnClickListener(this);
+        button_5.setOnClickListener(this);
+        button_6.setOnClickListener(this);
+        button_7.setOnClickListener(this);
+        button_8.setOnClickListener(this);
+        button_9.setOnClickListener(this);
+        button_0.setOnClickListener(this);
+        button_point.setOnClickListener(this);
+        button_add.setOnClickListener(this);
+        button_sub.setOnClickListener(this);
+        button_mult.setOnClickListener(this);
+        button_div.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
     }
 }
